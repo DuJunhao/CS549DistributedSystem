f22cdc00608e0f32f9b90a0bcfdcc09a3530d53b
DuJunhao <695973631@qq.com>
2019-05-17 18:51:19 -0400

    Add files via upload

---
+++ assignment3/Assignment3-Code/dht-streaming/src/edu/stevens/cs549/dhts/activity/UriApi.java
@@ -0,0 +1,16 @@
+package edu.stevens.cs549.dhts.activity;
+
+// added by Hongzheng Wang to encapsulate the REST API URI.
+public class UriApi {
+	public static String INFO = "/info";
+	public static String FIND_SUCCESSOR = "/find?id=%s";
+	public static String SUCC = "/succ";
+	public static String PRED = "/pred";
+	public static String GET_KEY_VALUE = "?key=%s";
+	public static String SET_KEY_VALUE = "?key=%s&val=%s";
+	public static String DELETE_KEY_VALUE = "?key=%s&val=%s";
+	public static String NOTIFY = "/notify";
+	public static String GET_NODE_BY_FINGER = "/finger?id=%s";
+	public static String LISTEN = "/listen?id=%s&key=%s";
+	
+}
